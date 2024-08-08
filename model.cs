3ec4ca45446fe0f71ec5e80bf8361cf5e96c8adc
yudha <yudha.dlesmana@gmail.com>
2024-08-08 14:04:40 +0700

    update 0.3

--- model.cs
+++ model.cs
@@ -1,13 +1,15 @@
+using System;
 
 public class Vehicle   
 {
     public VehicleType Tipe {get; set;}
     public string No {get; set;}
     public string Colour {get; set;}
+    public DateTime Park {get; set;}
+    // public DateTime Leave {get; set;}
 }
 public enum VehicleType
 {
     Mobil,
     Motor
 }
-
--- program.cs
+++ program.cs
@@ -13,20 +13,20 @@ public static void Main(string[] args)
 while(true){
 try
 {
-    int Lot = CreateParkingLot(); //tentukan jumlah lot
+    int Lot = CreateParkingLot(); 
 
-    while(true){ // infinite loop command
+    while(true){
     string[] commands = Input();
     string command = commands[0];
 
     switch(command){
         
         case "park":
-        { // command for adding vehicle to parkinglot
-        string[] arg = {commands[1],commands[2],commands[3]}; // collect arg for create vehicle
-        Vehicle vehicle = CekIn(arg); //create vehicle =
-        if(Parking.Count < Lot){ // add vehicle to parking lot is slot in it 
-            if(DeletedKey.Count>0){ // if there is 
+        string[] arg = {commands[1],commands[2],commands[3]}; 
+        Vehicle vehicle = CekIn(arg); 
+        vehicle.Park = DateTime.Now;
+        if(Parking.Count < Lot){ 
+            if(DeletedKey.Count>0){ 
                 int useDeletedKey = DeletedKey.First(); 
                 Parking[useDeletedKey] = vehicle;
                 Console.WriteLine($"Allocated slot number: {useDeletedKey}");
@@ -40,37 +40,40 @@ try
             Console.WriteLine("Sorry, parking lot is full");
         }
         break;
-        }
 
         case "leave": // remove vehicle from parkinglot
         int indx = int.Parse(commands[1]);
+        DateTime Leaving = DateTime.Now;
+        Vehicle Leave = Parking[indx];
         if (!DeletedKey.Contains(indx) && indx <= Lot){
             DeletedKey.Add(indx);
+            TimeSpan diff = Leaving.Subtract(Leave.Park);
+
+            Console.WriteLine($"{Leave.No} for {diff}");
+            Console.WriteLine($"Slot number {indx} is free ");
             Parking.Remove(indx);    
-            Console.WriteLine($"Slot number {indx} is free");
         }else{
             Console.WriteLine($"{indx} is empty");
         }
         break;
 
         case "status": 
-        Console.WriteLine("Slot \t No. \t \t Type \t Colour");
+        Console.WriteLine("Slot \t No. \t \t Type \t Colour  Parking");
         List<int> sortKey = Parking.Keys.ToList();
         sortKey.Sort();
         foreach(var k in sortKey)
         {
-             Console.WriteLine($"{k} \t {Parking[k].No} \t {Parking[k].Tipe} \t {Parking[k].Colour}");
+             Console.WriteLine(
+                $"{k} \t {Parking[k].No} \t {Parking[k].Tipe} \t {Parking[k].Colour} \t {Parking[k].Park}" );
         }
         break;
     }
-        if (command == "exit"){ //stop program 
-        break;
-        }
+    if (command == "exit"){
+    break;
+    }
     
     }
 }
-// jika input awal bukan bilangan akan memberi message "Wrong input, use numbers!!!" 
-// dan meminta input ulang untuk jumlah lot
 catch (System.FormatException)
 {
     Console.WriteLine("Wrong input, use numbers!!!");
@@ -80,82 +83,3 @@ catch (System.FormatException)
 }
 }
 }
-        //         case "type_of_vehicles":
-        //             string type = commands[1];
-        //             VehicleType vehicleT = 
-        //                 (VehicleType)Enum.Parse(typeof(VehicleType), type);
-        //             int hasil = 0;
-        //             foreach (var item in area){
-        //                 if(item.Value.Tipe == vehicleT){
-        //                     hasil++;
-        //                 }
-        //             }
-        //             Console.WriteLine(hasil);
-        //             break;
-        //         case "registration_numbers_for_vehicles_with_ood_plate":
-        //             List<string> listOdd = new List<string>{};
-        //             foreach (var item in area){
-        //                 string No = item.Value.No;
-        //                 string[] part = No.Split("-");
-        //                 int nomorOdd = int.Parse(part[1]);    
-        //                 if (nomorOdd%2 != 0){
-        //                     listOdd.Add(item.Value.No);
-        //                 }
-        //             }
-        //             string resultOdd = string.Join(", ", listOdd);
-        //             Console.WriteLine(resultOdd);
-
-        //             break;
-        //         case "registration_numbers_for_vehicles_with_event_plate":
-        //             List<string>listEvent = new List<string>{};
-        //             foreach (var item in area){
-        //                 string No = item.Value.No;
-        //                 string[] part = No.Split("-");
-        //                 int nomorEvent = int.Parse(part[1]);
-        //                 if (nomorEvent%2 == 0){
-        //                     listEvent.Add(item.Value.No);
-        //                 }
-        //             }
-        //             string resultEvent = string.Join(", ", listEvent);
-        //             Console.WriteLine(resultEvent);
-        //             break;
-        //         case "registration_numbers_for_vehicles_with_colour":
-        //             string warnaR = commands[1];
-        //             List<string> listColour = new List<string>{};
-        //             foreach (var item in area)
-        //             {
-        //                 if(warnaR == item.Value.Colour){
-        //                     listColour.Add(item.Value.No);
-        //                 }
-        //             }
-        //             string resultColour = string.Join(",", listColour);
-        //             Console.WriteLine(resultColour);
-        //             break;
-        //         case "slot_numbers_for_vehicles_with_colour":
-        //             string warnaS = commands[1];
-        //             List<int> listSlot = new List<int>{};
-        //             foreach (var item in area)
-        //             {
-        //                 if (warnaS == item.Value.Colour){
-        //                     listSlot.Add(item.Key);
-        //                 }
-        //             }
-        //             string resultSlot = string.Join(", ", listSlot);
-        //             Console.WriteLine(resultSlot);
-        //             Console.WriteLine(listSlot.Count);
-        //             break;
-        //         case "slot_number_for_registration_number":
-                    // string nomorReg = commands[1];
-                    // foreach(var item in area)
-                    // {
-                    //     if(nomorReg == item.Value.No){
-                    //         Console.WriteLine(item.Key);
-                    //     }else{
-                    //         Console.WriteLine("Not Found");
-                    //     }
-                    // }
-                    // break;
-        //     }
-        // }
-//     }
-// }
